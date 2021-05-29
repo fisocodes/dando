9cf397f43ee3fcabba9489bb5c12a4767e8a426d
Oscar Figueroa <fisocodes@gmail.com>
2021-05-28 22:28:10 -0700

    Fixes in weather widget

--- src/Weather/index.js
+++ src/Weather/index.js
@@ -13,7 +13,7 @@ class Weather extends Component{
     }
 
     async componentDidMount(){
-        const response = await fetch('http://api.weatherbit.io/v2.0/current?city=' + this.props.city + '&country='  + this.props.country + '&key=686c003b089b4468bc5fe1bc22d8ec49');
+        const response = await fetch('https://api.weatherbit.io/v2.0/current?city=' + this.props.city + '&country='  + this.props.country + '&key=686c003b089b4468bc5fe1bc22d8ec49');
         const responseJson = await response.json();
         console.log(responseJson);
         this.setState({
