dd5d37edd8612f0b2e00c9138c292211952f132f
Oscar Figueroa <fisocodes@gmail.com>
2021-08-15 16:32:55 -0700

    Sign Up fixes

--- src/Signup/index.js
+++ src/Signup/index.js
@@ -16,7 +16,7 @@ class Signup extends Component
                         <TextField className="form-input" type="password" label="Password" size="medium" color="secondary"/><br></br>
                         <TextField className="form-input" label="Name" size="medium" color="secondary"/><br></br>
                         <TextField className="form-input" label="Surname" size="medium" color="secondary"/><br></br>
-                        <TextField className="form-input" type="date" size="medium" color="secondary"/><br></br>
+                        <TextField className="form-input" type="date" label="DOB" size="medium" color="secondary"/><br></br>
                         <Button className="form-input" type="submit" variant="contained" color="primary">Log In</Button>
                     </form>
                 </Paper>
