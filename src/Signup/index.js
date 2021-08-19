330c8b84961622958d82544177bb1fa1c469e581
Oscar Figueroa <fisocodes@gmail.com>
2021-08-19 15:24:08 -0700

    First deploy of dando server

--- src/Signup/index.js
+++ src/Signup/index.js
@@ -1,22 +1,60 @@
 import React, {Component} from 'react';
 import { Paper, TextField, Button } from '@material-ui/core';
 
+import Axios from '../Axios';
+
 import './index.css';
 
 class Signup extends Component
 {
+    constructor(){
+        super();
+        this.state = {
+            username: "",
+            password: "",
+            name: "",
+            surname: "",
+            dob: "",
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value, 
+        });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        alert(`USER DATA\nUsername: ${this.state.username}\nPassword: ${this.state.password}\nName: ${this.state.name}\nSurname: ${this.state.surname}\nDOB: ${this.state.dob}\n`);
+
+        Axios.post('/users/create', {
+            username: this.state.username === '' ? null : this.state.username,
+            password: this.state.password === '' ? null : this.state.password,
+            name: this.state.name === '' ? null : this.state.name,
+            surname: this.state.surname === '' ? null : this.state.surname,
+            dob: this.state.dob === '' ? null : this.state.dob,
+        })
+        .then(function(response){
+            alert(response.data);
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+    }
+
     render()
     {
         return(
             <div className='login-form-wrapper'>
                 <Paper className="login-paper">
-                    <form className='login-form'>
+                    <form className='login-form' onSubmit={this.handleSubmit}>
                         <label className="form-input">Sign Up</label>
-                        <TextField className="form-input" autoFocus label="Username" size="medium" color="secondary"/><br></br>
-                        <TextField className="form-input" type="password" label="Password" size="medium" color="secondary"/><br></br>
-                        <TextField className="form-input" label="Name" size="medium" color="secondary"/><br></br>
-                        <TextField className="form-input" label="Surname" size="medium" color="secondary"/><br></br>
-                        <TextField className="form-input" type="date" label="DOB" size="medium" color="secondary"/><br></br>
+                        <TextField name="username" onChange={this.handleChange} className="form-input" autoFocus label="Username" size="medium" color="secondary"/><br></br>
+                        <TextField name="password" onChange={this.handleChange} className="form-input" type="password" label="Password" size="medium" color="secondary"/><br></br>
+                        <TextField name="name" onChange={this.handleChange} className="form-input" label="Name" size="medium" color="secondary"/><br></br>
+                        <TextField name="surname" onChange={this.handleChange} className="form-input" label="Surname" size="medium" color="secondary"/><br></br>
+                        <TextField name="dob" onChange={this.handleChange} className="form-input" type="date" label="DOB" size="medium" color="secondary"/><br></br>
                         <Button className="form-input" type="submit" variant="contained" color="primary">Log In</Button>
                     </form>
                 </Paper>
--- src/Star/index.js
+++ src/Star/index.js
@@ -5,63 +5,42 @@ import './index.css';
 
 class Star extends Component
 {
-  /*
-    componentDidMount()
-    {
-        let star = document.getElementsByClassName("starComponent");
+  render()
+  {
+    const starStyle = {
+      animationDelay: `${Math.random() * 8}s`,
+      animationDuration: `${2 + Math.random() * 8}s`,
 
-        for(let i = 0; i < star.length; i++)
+    }
+
+    const starWrapperStyle = {
+      top: `${Math.random() * 100}%`,
+      left: `${Math.random() * 100}%`,
+      transform: `scale(${0.25 + Math.random() * 0.75})`,
+    }
+
+    const parallaxData = [
         {
-            --let x = Math.floor(Math.random()*star[i].parentElement.offsetWidth);
-            --let y = Math.floor(Math.random()*star[i].parentElement.offsetHeight);
-            --let delay = Math.random()*8;
-            --let duration = 2 + Math.random()*8;
-            let scale = 0.25 + Math.random() * 0.5;
-            --star[i].style.left = x +'px';
-            --star[i].style.top = y-25 +'px';
-            --star[i].style.animationDelay = delay +'s';
-            --star[i].style.animationDuration = duration +'s';
-            star[i].style.transform = 'scale(' + scale + ')';
-        }
-    }
-    */
+          start: 0,
+          end: 1000,
+          properties: [
+            {
+              startValue: 0,
+              endValue: -Math.random() * 1000,
+              property: 'translateY',
+            },
+          ],
+        },
+      ];
 
-    render()
-    {
-      const starStyle = {
-        animationDelay: `${Math.random() * 8}s`,
-        animationDuration: `${2 + Math.random() * 8}s`,
-
-      }
-
-      const starWrapperStyle = {
-        top: Math.random() * 100 + '%',
-        left: Math.random() * 100 + '%',
-        transform: `scale(${0.25 + Math.random() * 0.75})`,
-      }
-
-      const parallaxData = [
-          {
-            start: 0,
-            end: 1000,
-            properties: [
-              {
-                startValue: 0,
-                endValue: -Math.random() * 1000,
-                property: 'translateY',
-              },
-            ],
-          },
-        ];
-
-      return(
-          <div className='star-wrapper' style={starWrapperStyle}>
-            <Plx  parallaxData={parallaxData}>
-                <div className='star-component' style={starStyle}></div>
-            </Plx>
-          </div>
-      );
-    }
+    return(
+        <div className='star-wrapper' style={starWrapperStyle}>
+          <Plx  parallaxData={parallaxData}>
+              <div className='star-component' style={starStyle}></div>
+          </Plx>
+        </div>
+    );
+  }
 }
 
 export default Star;
