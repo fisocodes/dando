46483fb5158b219e7119ddcdbb10ff5d5b1863b3
Oscar Figueroa <fisocodes@gmail.com>
2021-08-21 11:46:46 -0700

    Creating user feedback implemented

--- src/Signup/index.js
+++ src/Signup/index.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import { Paper, TextField, Button } from '@material-ui/core';
+import { Dialog, DialogTitle } from '@material-ui/core';
+import { CircularProgress} from '@material-ui/core';
 
 import Axios from '../Axios';
 
@@ -15,6 +17,7 @@ class Signup extends Component
             name: "",
             surname: "",
             dob: "",
+            isCreateDialogOpen: false,
         }
     }
 
@@ -28,6 +31,10 @@ class Signup extends Component
         event.preventDefault();
         alert(`USER DATA\nUsername: ${this.state.username}\nPassword: ${this.state.password}\nName: ${this.state.name}\nSurname: ${this.state.surname}\nDOB: ${this.state.dob}\n`);
 
+        this.setState({
+            isCreateDialogOpen: true,
+        });
+
         Axios.post('/users/create', {
             username: this.state.username === '' ? null : this.state.username,
             password: this.state.password === '' ? null : this.state.password,
@@ -35,7 +42,12 @@ class Signup extends Component
             surname: this.state.surname === '' ? null : this.state.surname,
             dob: this.state.dob === '' ? null : this.state.dob,
         })
-        .then(function(response){
+        .then((response) => {
+            
+            this.setState({
+                isCreateDialogOpen: false,
+            });
+
             alert(response.data);
         })
         .catch(function(error){
@@ -47,11 +59,15 @@ class Signup extends Component
     {
         return(
             <div className='login-form-wrapper'>
+                <Dialog open={this.state.isCreateDialogOpen}>
+                    <DialogTitle>Creating user</DialogTitle>
+                    <CircularProgress color="secondary"/>
+                </Dialog>
                 <Paper className="login-paper">
                     <form className='login-form' onSubmit={this.handleSubmit}>
                         <label className="form-input">Sign Up</label>
-                        <TextField name="username" onChange={this.handleChange} className="form-input" autoFocus label="Username" size="medium" color="secondary"/><br></br>
-                        <TextField name="password" onChange={this.handleChange} className="form-input" type="password" label="Password" size="medium" color="secondary"/><br></br>
+                        <TextField name="username" required onChange={this.handleChange} className="form-input" autoFocus label="Username" size="medium" color="secondary"/><br></br>
+                        <TextField name="password" required onChange={this.handleChange} className="form-input" type="password" label="Password" size="medium" color="secondary"/><br></br>
                         <TextField name="name" onChange={this.handleChange} className="form-input" label="Name" size="medium" color="secondary"/><br></br>
                         <TextField name="surname" onChange={this.handleChange} className="form-input" label="Surname" size="medium" color="secondary"/><br></br>
                         <TextField name="dob" onChange={this.handleChange} className="form-input" type="date" label="DOB" size="medium" color="secondary"/><br></br>
