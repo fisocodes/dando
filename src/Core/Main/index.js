c5cce9d0a7774156250676e8a68a1f2398719527
Oscar Figueroa <fisocodes@gmail.com>
2021-09-17 15:09:31 -0700

    Sessions implemented

--- src/Core/Main/index.js
+++ src/Core/Main/index.js
@@ -3,17 +3,34 @@ import React, {Component} from 'react';
 import Landing from '../../Core/Landing';
 import Client from '../../Core/Client';
 
+import Axios from '../Axios';
+
 class Main extends Component{
 
     constructor(){
         super();
+
         this.state = {
-            isAuth: false,
+            username: 'fisocodes',
+            password: 'sotooscar1',
+            user: null,
         };
+
+        Axios.post('/users/authenticate', {
+            username: this.state.username === '' ? null : this.state.username,
+            password: this.state.password === '' ? null : this.state.password,
+        }, {withCredentials: true})
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((error) =>{
+            console.log(error);
+        });
+
     }
 
     render(){
-        return this.state.isAuth ? <Client/> : <Landing/>;
+        return this.state.user === null ? <Landing/> : <Client/>;
     }
 }
 
