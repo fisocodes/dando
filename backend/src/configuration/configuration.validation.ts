ccac784550fba7de718d3102400dce657f93b166
Oscar Figueroa <fisocodes@gmail.com>
2026-05-22 12:25:59 -0700

    feat(backend): added configuration module

---
+++ backend/src/configuration/configuration.validation.ts
@@ -0,0 +1,17 @@
+import { plainToInstance } from "class-transformer";
+import { validateSync } from "class-validator";
+
+export class EnvironmentVariables {}
+
+export function validate(config: Record<string, unknown>) {
+	const validatedConfig = plainToInstance(EnvironmentVariables, config, {
+		enableImplicitConversion: true,
+	});
+
+	const errors = validateSync(validatedConfig, {
+		whitelist: true,
+	});
+
+	if (errors.length > 0) throw new Error(errors.toString());
+	return validatedConfig;
+}
